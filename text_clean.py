93f4fc129dc146405f6b0dfd9681bc9d5513f1d6
khub41 <62697244+khub41@users.noreply.github.com>
2021-08-17 12:30:15 +0200

    feature engineering
    in progress
    TSNE viz ok

--- text_clean.py
+++ text_clean.py
@@ -2,6 +2,12 @@ import pandas as pd
 import nltk
 from nltk.corpus import stopwords
 from nltk.stem.snowball import SnowballStemmer
+from nltk.stem import WordNetLemmatizer
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.manifold import TSNE
+from sklearn.decomposition import PCA
+from sklearn.preprocessing import StandardScaler
+
 
 import udf
 
@@ -33,7 +39,7 @@ print("Size of the repertory : ", len(all_words.unique()))
 # text cleaning
 # Exclude non alphanumerical text
 
-data.description_tok = data.description.apply(udf.clean_non_alphanum)
+data.description_tok = data.description.apply(udf.tokenize_custom)
 
 all_words = udf.get_all_words(data.description_tok)
 # all_words.value_counts().head(10).plot(kind='pie', title='10 most common "words"')
@@ -41,10 +47,7 @@ print("Size of the repertory after alphanumeric filter : ", len(all_words.unique
 
 # Deleting most frequent words
 
-# print(all_words.value_counts().head(40))
-data.description_tok = data.description_tok.apply(udf.delete_words,
-                                                  frequent_words=all_words.value_counts().head(40).index)
-#TODO Pas besoin de faire ça : on peut faire avec tf et idf pour pénaliser les tokens fréquents
+# We'll use tf and idf to penalize most frequent tokens. Deleting the X most frequent tokens is too arbitrary
 
 all_words = udf.get_all_words(data.description_tok)
 print("Size of the repertory after most common words filter : ", len(all_words.unique()))
@@ -58,13 +61,228 @@ print("Size of the repertory after stop words filter : ", len(all_words.unique()
 # TODO Attention aux stopwords par défaut
 
 # Stemming
-# TODO faire plutot du lemmatization (moins radical)
-stemmer_eng = SnowballStemmer("english")
 
-data.description_tok = data.description_tok.apply(udf.stem_string,
-                                                  stemmer=stemmer_eng)
+lemmatizer_eng = WordNetLemmatizer()
+
+data.description_tok = data.description_tok.apply(udf.lem_string,
+                                                  lemmatizer=lemmatizer_eng)
 all_words = udf.get_all_words(data.description_tok)
-print("Size of the repertory after stemming : ", len(all_words.unique()))
+print("Size of the repertory after lemmatization : ", len(all_words.unique()))
 
-# TODO evaluer les catégories
+#TODO test stemm when connection is better
+
+# Convert token sequences into bigram sequences
+
+data['description_bigram'] = data.description_tok.apply(udf.get_bigrams)
+
+
+#TODO evaluer les catégories
+
+data['label_categ_0'] = data.product_category_tree.apply(udf.process_categs,
+                                                         level=0)
+# 7 categs with uniform distribution is yummmmy.
+# When going one level deeper in the tree we get 62 categs with lot having one item
+
+
+def custom_tokenize(text):
+    tokens = udf.tokenize_custom(text)
+
+    lemmatizer_eng = WordNetLemmatizer()
+    tokens_stem = udf.lem_string(tokens, lemmatizer=lemmatizer_eng)
+    return tokens_stem
+
 # TODO faire un transfert learning? VGGNET/RESNET images. BERT : texte
+
+sw = [
+ 'yourselves',
+ "should've",
+
+
+ 'yours',
+ 'to',
+
+
+ 'those',
+ 'myself',
+ 's',
+ "you're",
+ 'ourselves',
+
+ 've',
+
+ "hadn't",
+ 'here',
+ 'of',
+
+
+
+
+
+
+ 'on',
+ 're',
+
+ 'y',
+ 'my',
+
+
+
+ 'that',
+
+ 'doing',
+
+
+ 'd',
+ "she's",
+ 'theirs',
+ 'you',
+ 'having',
+
+ 'has',
+
+ 'been',
+ "shouldn't",
+ 'through',
+ 'your',
+ 'all',
+ 'couldn',
+ "didn't",
+ 'this',
+ 'i',
+ 'these',
+
+
+
+ 'needn',
+ 'why',
+ 'm',
+ 'was',
+
+
+ 'we',
+ 'just',
+ 'now',
+ 'how',
+ 'as',
+
+ 'more',
+ "won't",
+ 'are',
+ 'does',
+ 'if',
+ 'his',
+ 'ain',
+ 'himself',
+ 'be',
+ 'shouldn',
+ 'they',
+ 'me',
+ 'aren',
+ 'which',
+ 'during',
+
+ 'such',
+ "couldn't",
+ "mustn't",
+ "you'd",
+
+ 'then',
+ 'will',
+
+ 'he',
+ "that'll",
+ 'and',
+ 'but',
+ "needn't",
+ 'being',
+ 'from',
+ 'll',
+
+ 'hadn',
+ 'o',
+ 'themselves',
+ 'or',
+
+ 'the',
+
+
+
+ 'haven',
+
+ 'for',
+ 'what',
+ 'ma',
+ 'their',
+ "doesn't",
+ 'shan',
+ 'our',
+
+ "hasn't",
+ 'yourself',
+
+
+
+ 't',
+ "you'll",
+ 'am',
+ 'were',
+ 'while',
+ 'who',
+ "haven't",
+ 'mightn',
+ 'down',
+ 'she',
+ "wasn't",
+ 'again',
+ 'itself',
+ 'isn',
+ 'mustn',
+ "it's",
+ "weren't",
+ "don't",
+
+
+ 'further',
+
+ 'hasn',
+ 'nor',
+ "you've",
+ 'it',
+ 'wasn',
+ 'them',
+ 'can',
+ 'have',
+
+ 'a',
+ "mightn't",
+ 'until',
+
+ 'weren',
+ 'by',
+ 'whom',
+
+ "wouldn't",
+ 'when',
+ 'is',
+ 'an',
+ 'don',
+ 'herself',
+
+ 'its']
+tfidf = TfidfVectorizer(tokenizer=custom_tokenize, stop_words=sw, ngram_range=(1,2))
+values = tfidf.fit_transform(data.description)
+
+dense = values.todense()
+feature_names = tfidf.get_feature_names()
+denselist = dense.tolist()
+data_raw = pd.DataFrame(denselist, columns=feature_names).reset_index()
+
+data_full = data.merge(data_raw, how='left', right_on='index', left_index=True)
+
+data_scale = udf.scale_data(data_raw)
+
+udf.scree_plot(data_scale, 1050, savefig=False)
+
+data_scale_decomp = udf.reduce_dim_pca(data_scale, 700)
+
+data_tsne = udf.train_tsne(data_scale_decomp, data.label_categ_0)
--- udf.py
+++ udf.py
@@ -1,6 +1,10 @@
 import pandas as pd
 import nltk
-
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+from sklearn.preprocessing import StandardScaler
 
 def short_in_long_rate(row):
     """
@@ -28,10 +32,13 @@ def get_all_words(descriptions):
     return all_words
 
 
-def clean_non_alphanum(text):
-    tokenizer = nltk.RegexpTokenizer(r'\w+')
+def tokenize_custom(text):
+    tokenizer = nltk.RegexpTokenizer(r'\w+') #TODO modifier pour enlever les chiffres
+    words = tokenizer.tokenize(text)
+    words = [word.lower() for word in words if word.isalpha()]
 
-    return tokenizer.tokenize(text)
+    return words
+
 
 
 def delete_words(words, frequent_words):
@@ -44,3 +51,69 @@ def stem_string(words, stemmer):
     words_stemmed = [stemmer.stem(word) for word in words]
 
     return words_stemmed
+
+def lem_string(words, lemmatizer):
+    words_lemz = [lemmatizer.lemmatize(word) for word in words]
+
+    return words_lemz
+
+def process_categs(product_category_tree, level=0):
+    product_category_tree = product_category_tree.strip('[')
+    product_category_tree = product_category_tree.strip(']')
+    product_category_tree = product_category_tree.strip('"')
+    product_category_tree = product_category_tree.split(' >> ')
+    try:
+        return(product_category_tree[level])
+    except IndexError:
+        n_categs = len(product_category_tree)
+        print(f"Category tree only has {n_categs} categs, level must be between 0 and {n_categs}")
+
+def get_bigrams(tokens):
+    return list(nltk.bigrams(tokens))
+
+
+def scale_data(raw_features):
+    scaler = StandardScaler()
+
+    data_scale = scaler.fit_transform(raw_features)
+
+    return data_scale
+
+def scree_plot(data_scale, max_comp, savefig=False):
+    pca_scree = PCA(n_components=max_comp)
+    pca_scree.fit(data_scale)
+    plt.style.use('fivethirtyeight')
+    plt.xlabel('Nb de Composantes Principales')
+    plt.ylabel('Pourcentage de variance expliquée (cumulée)')
+    plt.title('Scree plot PCA')
+    plt.plot(np.arange(1, pca_scree.n_components_ + 1), pca_scree.explained_variance_ratio_.cumsum() * 100,
+             color='#8c704d')
+    if savefig:
+     plt.savefig('plots/{}.png'.format(savefig), bbox_inches='tight', dpi=720)
+    plt.show()
+
+
+def reduce_dim_pca(data_scale, n_comp):
+    pca = PCA(n_components=n_comp)
+    data_scale_decomp = pca.fit_transform(data_scale)
+
+    return data_scale_decomp
+
+
+def train_tsne(data, labels, perplexity=50, show=True, savefig=False):
+    tsne = TSNE(init='pca', random_state=41, n_jobs=-1, perplexity=perplexity)
+    data_tsne = tsne.fit_transform(data)
+    df_data_tsne_labels = pd.DataFrame(data_tsne, columns=['x', 'y']).merge(labels, left_index=True, right_index=True)
+    if show:
+        plt.style.use('seaborn')
+        plt.figure(figsize=(8, 8))
+        plt.axis('equal')
+        for label in labels.unique():
+            group = df_data_tsne_labels[df_data_tsne_labels[labels.name] == label]
+            plt.scatter(group.x, group.y, label=label)
+        plt.legend()
+        plt.show=()
+        if savefig:
+            plt.savefig('plots/{}.png'.format(savefig), bbox_inches='tight', dpi=720)
+
+    return data_tsne
