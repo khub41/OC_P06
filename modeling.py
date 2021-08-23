bfc130d4d26777f30e32d3a238497ea0fe108f90
khub41 <62697244+khub41@users.noreply.github.com>
2021-08-23 19:27:47 +0200

    Create modeling.py
    Kmeans and supervised

---
+++ modeling.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+from sklearn.ensemble import GradientBoostingClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import f1_score, confusion_matrix
+data = pd.read_csv('data/decomp_2308_700comp.csv', index_col=[0])
+
+def train_model_kmeans(data_train, n_clusters):
+    data_train_copy = data_train.copy()
+    model = KMeans(n_clusters=n_clusters, init='random')
+    predicted_labels = model.fit_predict(data_train_copy.drop(columns=['label']))
+    data_train_copy['predicted_label'] = predicted_labels
+
+    return data_train_copy
+
+def train_model_GB(data_train):
+    data_train_copy = data_train.copy()
+
+    X_train, X_test, y_train, y_test = \
+        train_test_split(data_train.drop(columns=['label']),
+                     data_train.label,
+                     test_size=0.3,
+                     random_state=41)
+    model = GradientBoostingClassifier()
+    model.fit(X_train, y_train)
+
+    y_pred = model.predict(X_test)
+    score = f1_score(y_test, y_pred)
+    matrix_confuse = confusion_matrix(y_test, y_pred)
+    return matrix_confuse
+
+
+new_data = train_model_kmeans(data, 7)
+
+confusion_GB = train_model_GB(data)
