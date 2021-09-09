fadac1cea86b096d66c05ae832e20d6a5470787f
khub41 <62697244+khub41@users.noreply.github.com>
2021-09-09 17:29:47 +0200

    end 09/09
    image clean ok init modeling descriptor computed

--- image_clean.py
+++ image_clean.py
@@ -4,6 +4,9 @@ import cv2
 import matplotlib.pyplot as plt
 import os
 
+PATH = 'data/Flipkart/'
+DATA_FILE_NAME = ''
+
 
 def show_img(img):
     """Convenience function to display a typical color image"""
@@ -11,44 +14,55 @@ def show_img(img):
 
 
 def to_gray(color_img):
+    """Converts a RGB image into a gray scale image"""
     gray = cv2.cvtColor(color_img, cv2.COLOR_BGR2GRAY)
     return gray
 
 
 def gen_sift_features(gray_img):
+    """Converts an image into keypoints and dscriptors (128 dimensional vectors)"""
     sift = cv2.SIFT_create()
-    # kp is the keypoints
-    #
-    # desc is the SIFT descriptors, they're 128-dimensional vectors
-    # that we can use for our final features
+
     kp, desc = sift.detectAndCompute(gray_img, None)
     return kp, desc
 
 
 def show_sift_features(gray_img, color_img, kp):
+    """Show the keypoints on the original picture"""
     return plt.imshow(cv2.drawKeypoints(gray_img, kp, color_img.copy()))
 
 
-data = pd.read_csv('data/Flipkart/flipkart_com-ecommerce_sample_1050.csv')
-
-product = data.sample(1, random_state=41)
-
-img = cv2.imread('data/Flipkart/Images/' + product.image.iloc[0])
-# show_img(img)
-
-img_bw = to_gray(img)
-# show_img(img_bw)
-
-img_bw_kp, img_bw_desc = gen_sift_features(img_bw)
-
-show_sift_features(img_bw, img, img_bw_kp)
+def get_descriptors(filename):
+    """From a filename, gets the list of descriptors"""
+    img = cv2.imread(filename)
+    img_bw = to_gray(img)
+    img_bw_kp, img_bw_desc = gen_sift_features(img_bw)
+    return img_bw_desc
 
 
+def main():
+    data = pd.read_csv('data/Flipkart/flipkart_com-ecommerce_sample_1050.csv')
+
+    product = data.sample(1)
+
+    img = cv2.imread('data/Flipkart/Images/' + product.image.iloc[0])
+    # show_img(img)
+
+    img_bw = to_gray(img)
+    # show_img(img_bw)
+
+    img_bw_kp, img_bw_desc = gen_sift_features(img_bw)
+
+    show_sift_features(img_bw, img, img_bw_kp)
 
 
-sift = cv2.SIFT_create()
-kp = sift.detect(img_bw,None)
-img_bw = cv2.drawKeypoints(img_bw, kp, img)
+if __name__ == '__main__':
+    main()
+
+
+# sift = cv2.SIFT_create()
+# kp = sift.detect(img_bw,None)
+# img_bw = cv2.drawKeypoints(img_bw, kp, img)
 
 
 
---
+++ image_modeling.py
@@ -0,0 +1,31 @@
+import image_clean
+import pandas as pd
+from sklearn.preprocessing import StandardScaler
+from sklearn.cluster import KMeans
+import os
+
+PATH_IMAGES = 'data/Flipkart/Images'
+
+
+def collect_all_features():
+    original_dir = os.getcwd()
+    os.chdir(PATH_IMAGES)
+
+    full_features = pd.DataFrame()
+    for file_name in os.listdir():
+        descriptors = image_clean.get_descriptors(file_name)
+
+        file_df = pd.DataFrame(descriptors)
+        try:
+            file_df['image_id'] = pd.Series([file_name] * len(descriptors))
+            full_features = full_features.append(file_df)
+        except TypeError as e:
+            print(e)
+            print(file_name)
+
+    return full_features
+
+
+data_img = collect_all_features()
+scaler = StandardScaler()
+data_img_scale = scaler.fit_transform(data_img.drop(columns=['image_id']))
