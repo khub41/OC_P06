6eff941d4ff0faf7bd3c51be7a68cb2a0d13b318
khub41 <62697244+khub41@users.noreply.github.com>
2021-09-09 10:07:47 +0200

    Update text_clean.py
    before deleting bilel's list

--- text_clean.py
+++ text_clean.py
@@ -9,237 +9,256 @@ from sklearn.manifold import TSNE
 from sklearn.decomposition import PCA
 from sklearn.preprocessing import StandardScaler
 
-
 import udf
 
 data = pd.read_csv('data/Flipkart/flipkart_com-ecommerce_sample_1050.csv')
-sw_nltk_bilel = [
- "key",
- "feature",
- 'product',
- 'yourselves',
- "should've",
- 'yours',
- 'to',
- 'those',
- 'myself',
- 's',
- "you're",
- 'ourselves',
- 've',
- "hadn't",
- 'here',
- 'of',
- 'on',
- 're',
- 'y',
- 'my',
- 'that',
- 'doing',
- 'd',
- "she's",
- 'theirs',
- 'you',
- 'having',
- 'has',
- 'been',
- "shouldn't",
- 'through',
- 'your',
- 'all',
- 'couldn',
- "didn't",
- 'this',
- 'i',
- 'these',
- 'needn',
- 'why',
- 'm',
- 'was',
- 'we',
- 'just',
- 'now',
- 'how',
- 'as',
- 'more',
- "won't",
- 'are',
- 'does',
- 'if',
- 'his',
- 'ain',
- 'himself',
- 'be',
- 'shouldn',
- 'they',
- 'me',
- 'aren',
- 'which',
- 'during',
- 'such',
- "couldn't",
- "mustn't",
- "you'd",
- 'then',
- 'will',
- 'he',
- "that'll",
- 'and',
- 'but',
- "needn't",
- 'being',
- 'from',
- 'll',
- 'hadn',
- 'o',
- 'themselves',
- 'or',
- 'the',
- 'haven',
- 'for',
- 'what',
- 'ma',
- 'their',
- "doesn't",
- 'shan',
- 'our',
- "hasn't",
- 'yourself',
- 't',
- "you'll",
- 'am',
- 'were',
- 'while',
- 'who',
- "haven't",
- 'mightn',
- 'down',
- 'she',
- "wasn't",
- 'again',
- 'itself',
- 'isn',
- 'mustn',
- "it's",
- "weren't",
- "don't",
- 'further',
- 'hasn',
- 'nor',
- "you've",
- 'it',
- 'wasn',
- 'them',
- 'can',
- 'have',
- 'a',
- "mightn't",
- 'until',
- 'r',
- 'weren',
- 'by',
- 'whom',
- "wouldn't",
- 'when',
- 'is',
- 'an',
- 'don',
- 'herself',
- 'its']
-sw_detected = ['in', 'genuine', 'day', 'buy', 'shipping', 'guarantee', 'at', 'cash',
-               'delivery', 'with', 'free', 'only', 'online', 'price', 'replacement', 'com', 'flipkart']
+
+# Stop words. Stop words are
+sw_nltk = stopwords.words('english')
+sw_nltk_custom = [
+    "key",
+    "feature",
+    'product',
+    'yourselves',
+    "should've",
+    'yours',
+    'to',
+    'those',
+    'myself',
+    's',
+    "you're",
+    'ourselves',
+    've',
+    "hadn't",
+    'here',
+    'of',
+    'on',
+    're',
+    'y',
+    'my',
+    'that',
+    'doing',
+    'd',
+    "she's",
+    'theirs',
+    'you',
+    'having',
+    'has',
+    'been',
+    "shouldn't",
+    'through',
+    'your',
+    'all',
+    'couldn',
+    "didn't",
+    'this',
+    'i',
+    'these',
+    'needn',
+    'why',
+    'm',
+    'was',
+    'we',
+    'just',
+    'now',
+    'how',
+    'as',
+    'more',
+    "won't",
+    'are',
+    'does',
+    'if',
+    'his',
+    'ain',
+    'himself',
+    'be',
+    'shouldn',
+    'they',
+    'me',
+    'aren',
+    'which',
+    'during',
+    'such',
+    "couldn't",
+    "mustn't",
+    "you'd",
+    'then',
+    'will',
+    'he',
+    "that'll",
+    'and',
+    'but',
+    "needn't",
+    'being',
+    'from',
+    'll',
+    'hadn',
+    'o',
+    'themselves',
+    'or',
+    'the',
+    'haven',
+    'for',
+    'what',
+    'ma',
+    'their',
+    "doesn't",
+    'shan',
+    'our',
+    "hasn't",
+    'yourself',
+    't',
+    "you'll",
+    'am',
+    'were',
+    'while',
+    'who',
+    "haven't",
+    'mightn',
+    'down',
+    'she',
+    "wasn't",
+    'again',
+    'itself',
+    'isn',
+    'mustn',
+    "it's",
+    "weren't",
+    "don't",
+    'further',
+    'hasn',
+    'nor',
+    "you've",
+    'it',
+    'wasn',
+    'them',
+    'can',
+    'have',
+    'a',
+    "mightn't",
+    'until',
+    'r',
+    'weren',
+    'by',
+    'whom',
+    "wouldn't",
+    'when',
+    'is',
+    'an',
+    'don',
+    'herself',
+    'its']
+sw_to_keep_anyway = ['ours',
+                     'him',
+                     'her',
+                     'hers',
+                     'had',
+                     'do',
+                     'did',
+                     'because',
+                     'at',
+                     'with',
+                     'about',
+                     'against',
+                     'between',
+                     'into',
+                     'before',
+                     'after',
+                     'above',
+                     'below',
+                     'up',
+                     'in',
+                     'out',
+                     'off',
+                     'over',
+                     'under',
+                     'once',
+                     'there',
+                     'where',
+                     'any',
+                     'both',
+                     'each',
+                     'few',
+                     'most',
+                     'other',
+                     'some',
+                     'no',
+                     'not',
+                     'only',
+                     'own',
+                     'same',
+                     'so',
+                     'than',
+                     'too',
+                     'very',
+                     'should',
+                     "aren't",
+                     'didn',
+                     'doesn',
+                     "isn't",
+                     "shan't",
+                     'won',
+                     'wouldn']
+sw_nltk_custom_bis = [word for word in sw_nltk if word not in sw_to_keep_anyway]
+# See the end of the script to understand the detected stopwords or go to udf.detect_useless_words
+sw_detected = ['cash',
+ 'with',
+ 'flipkart',
+ 'delivery',
+ 'at',
+ 'guarantee',
+ 'day',
+ 'replacement',
+ 'in',
+ 'buy',
+ 'com',
+ 'genuine',
+ 'free',
+ 'shipping',
+ 'price',
+ 'online',
+ 'only']
 # sw_detected = []
-sw = sw_nltk_bilel + sw_detected
-# # Normalizing capital letters:
-#
-# data['description'] = data['description'].apply(lambda x : x.lower())
-# data['product_name'] = data['product_name'].apply(lambda x : x.lower())
-# # Tokenizing name and description distinctly
-#
-# data['description_tok'] = data['description'].apply(nltk.word_tokenize)
-# data['name_tok'] = data['product_name'].apply(nltk.word_tokenize)
-#
-# # Comparing tokenized data
-#
-# data['simil_rate_texts'] = data.apply(udf.short_in_long_rate, axis=1)
-# print(data['simil_rate_texts'].describe(),
-#       '\nWe can use only the description as very few data from the name is not present in it')
-#
-# # Exploring data
-#
-#
-# all_words = udf.get_all_words(data.description_tok)
-# # all_words.value_counts().head(10).plot(kind='pie', title='10 most common "words"')
-#
-# print("Size of the repertory : ", len(all_words.unique()))
-#
-# # text cleaning
-# # Exclude non alphanumerical text
-#
-# data.description_tok = data.description.apply(udf.tokenize_custom)
-#
-# all_words = udf.get_all_words(data.description_tok)
-# # all_words.value_counts().head(10).plot(kind='pie', title='10 most common "words"')
-# print("Size of the repertory after alphanumeric filter : ", len(all_words.unique()))
-#
-# # Deleting most frequent words
-#
-# # We'll use tf and idf to penalize most frequent tokens. Deleting the X most frequent tokens is too arbitrary
-#
-# all_words = udf.get_all_words(data.description_tok)
-# print("Size of the repertory after most common words filter : ", len(all_words.unique()))
-#
-# # Deleting stopwords
-# data.description_tok = data.description_tok.apply(udf.delete_words,
-#                                                   frequent_words=sw)
-# all_words = udf.get_all_words(data.description_tok)
-# print("Size of the repertory after stop words filter : ", len(all_words.unique()))
-#
-#
-#
-# # Stemming
-#
-# lemmatizer_eng = WordNetLemmatizer()
-#
-# data.description_tok = data.description_tok.apply(udf.lem_string,
-#                                                   lemmatizer=lemmatizer_eng)
-# all_words = udf.get_all_words(data.description_tok)
-# print("Size of the repertory after lemmatization : ", len(all_words.unique()))
-#
-#
-# # Convert token sequences into bigram sequences
-#
-# data['description_bigram'] = data.description_tok.apply(udf.get_bigrams)
+sw = sw_nltk_custom_bis + sw_detected
 
-
-
+# Get Categories
 data['label_categ_0'] = data.product_category_tree.apply(udf.process_categs,
                                                          level=0)
-# 7 categs with uniform distribution is yummmmy.
+
+
+#  have 7 categs with uniform distribution
 # When going one level deeper in the tree we get 62 categs with lot having one item
 
+# We are going to use TFIDF Vectorizer to create the features.
+# This Sklearn object uses a tokenizing function and creates the n_grams
 
 def custom_tokenize(text):
-    #Creation of alphanum tokens
+    """
+    Tokenizes, deletes the words that are'nt fully alphabetical and apply a lemmatizer
+
+    :param text: str. The text we need to tokenize into simple tokens
+    :return: tokens_lem: list.
+    """
+
+    # Creation of alphanum tokens
     tokenizer = nltk.RegexpTokenizer(r'\w+')
     words = tokenizer.tokenize(text)
 
-    #Deleting numbers
+    # Deleting numbers
     words = [word.lower() for word in words if word.isalpha()]
 
-    #Getting lemms
+    # Getting lemms
     lemmatizer_eng = WordNetLemmatizer()
-    tokens_stem = udf.lem_string(words, lemmatizer=lemmatizer_eng)
+    tokens_lem = udf.lem_string(words, lemmatizer=lemmatizer_eng)
 
-    return tokens_stem
+    return tokens_lem
 
-# TODO faire un transfert learning? VGGNET/RESNET images. BERT : texte
 
 data['description_tok'] = data.description.apply(custom_tokenize)
 data['description_tok'] = data.description_tok.apply(udf.delete_words, frequent_words=sw)
 all_words = udf.get_all_words(data.description_tok)
 
-tfidf = TfidfVectorizer(tokenizer=custom_tokenize, stop_words=sw, ngram_range=(1,2))
+tfidf = TfidfVectorizer(tokenizer=custom_tokenize, stop_words=sw, ngram_range=(1, 2))
 values = tfidf.fit_transform(data.description)
 
 dense = values.todense()
@@ -259,5 +278,8 @@ data_tsne = udf.train_tsne(data_scale_decomp, data.label_categ_0, learning_rate=
 
 # most_baby = udf.get_most_frequent_per_categ(data, 'Baby Care', 20)
 
-# df_useless = udf.detect_useless_words(data, 20)
+df_useless = udf.detect_useless_words(data, 20)
+detected_useless = list(df_useless[df_useless.nb_apperances >=3].index)
 
+
+# TODO faire un transfert learning? VGGNET/RESNET images. BERT : texte
